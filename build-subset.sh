e613f6e7f84e0dfc378e154899e5e522e0f03fe2
江夏尧 <3446798488@qq.com>
2024-07-13 16:52:42 +0800

    fix: 修复 subset 构建的 bug

--- build-subset.sh
+++ build-subset.sh
@@ -14,7 +14,12 @@ em++ \
 	-DHB_CONFIG_OVERRIDE_H=\"config-override-subset.h\" \
 	-DHB_EXPERIMENTAL_API \
 	--no-entry \
+	-s MODULARIZE \
+	-s EXPORT_ES6=1 \
 	-s EXPORTED_FUNCTIONS=@hb-subset.symbols \
+	-s EXPORTED_RUNTIME_METHODS='["addFunction", "wasmMemory", "wasmExports"]' \
 	-s INITIAL_MEMORY=65MB \
-	-o hb-subset.wasm \
+	-s ALLOW_TABLE_GROWTH \
+	-lexports.js \
+	-o hb-subset.js \
 	hbjs-subset.cc
